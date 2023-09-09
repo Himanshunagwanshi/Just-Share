3060b3f66fe8272d8ab3cbaac5512c7dfe8aa901
Himanshu Nagwanshi <himanshunagwanshi90@gmail.com>
2023-09-09 16:10:47 +0530

    backend

---
+++ config/db.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+function connecDB() {
+  //Database Connection
+
+  mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+   // useCreateIndex: true,
+  });
+  const connection = mongoose.connection;
+connection
+  .once("open", function () {
+    console.log("Database Connected");
+  })
+  .on("error", function (err) {
+    console.log(err);
+  });
+}
+module.exports = connecDB;
